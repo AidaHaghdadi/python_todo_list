89b3551bad93f8a4f172208ec619765e8ef0214b
Aida <aida.hg0483@gmail.com>
2026-06-23 16:44:20 +0330

    create basic menu loop

---
+++ python_todo_list.py
@@ -0,0 +1,15 @@
+
+tasks = []
+
+user_choose = None
+while user_choose != 4 :
+    print("1-Add Task")
+    print("2-Show Tasks")
+    print("3-Delete Task")
+    print("4-Exit")
+
+    user_choose = int(input("Enter your choose:"))
+
+if user_choose == 4 :
+    print("Goodby:)")
+
