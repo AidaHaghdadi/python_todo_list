7198741931f586a11c3f48db977c627af1d05697
Aida <aida.hg0483@gmail.com>
2026-06-24 14:55:03 +0330

    Complete to-do list application

--- python_todo_list.py
+++ python_todo_list.py
@@ -9,8 +9,12 @@ while user_choose != 4 :
     print("3-Delete Task")
     print("4-Exit\n")
 
-    user_choose = int(input("Enter your choose:"))
-
+    try :
+        user_choose = int(input("Enter your choose:"))
+    except ValueError :
+        print("Invalid input!")
+        continue
+    
     if user_choose == 1 :
         user_task = input("Enter your task :")
         tasks.append(user_task)
@@ -28,15 +32,27 @@ while user_choose != 4 :
         if len(tasks) != 0 :
             for index, task in enumerate(tasks):
                 print(f"{index + 1}. {task}")
-            task_number_for_delete = int(input("\nEnter task number to delete :"))
-            task_number_for_delete -= 1
-            tasks.pop(task_number_for_delete)
-            print("Task deleted!\n")
-            print("New tasks list :")
-            for index, task in enumerate(tasks):
-                print(f"{index + 1}. {task}")
+            try :
+                task_number_for_delete = int(input("\nEnter task number to delete :"))
+                if 1 <= task_number_for_delete <= len(tasks) :
+                    task_number_for_delete -= 1
+                    tasks.pop(task_number_for_delete)
+                    print("Task deleted!\n")
+                    if tasks :
+                        print("New tasks list :")
+                        for index, task in enumerate(tasks):
+                            print(f"{index + 1}. {task}")
+                    if not tasks:
+                        print("No tasks found!")
+                else :
+                    print("Invalid task number!")
+            except ValueError :
+                print("Please enter a number!")
         else : 
-            print("No tasks list found!")
+            print("No tasks found!")
 
-if user_choose == 4 :
-    print("Goodby:)")
+    elif user_choose == 4 :
+        print("Goodbye :)") 
+
+    else:
+        print("Invalid choice!")
