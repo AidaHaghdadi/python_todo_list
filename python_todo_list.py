e05e94138a97250b60278b943cbccd0e85155cf8
Aida <aida.hg0483@gmail.com>
2026-06-24 13:54:48 +0330

    Add task creation and display

--- python_todo_list.py
+++ python_todo_list.py
@@ -3,12 +3,18 @@ tasks = []
 
 user_choose = None
 while user_choose != 4 :
+    print("\nMenu:")
     print("1-Add Task")
     print("2-Show Tasks")
     print("3-Delete Task")
-    print("4-Exit")
+    print("4-Exit\n")
 
     user_choose = int(input("Enter your choose:"))
+
+    if user_choose == 1 :
+        user_task = input("Enter your task :")
+        tasks.append(user_task)
+        print("Task added!\n")
 
 if user_choose == 4 :
     print("Goodby:)")
